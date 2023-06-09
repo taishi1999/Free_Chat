7ca167502a31263cbd1c93f27015e2c81de21c5c
karasaki <tkkong1@gmail.com>
2023-06-09 19:57:16 +0900

    profile.dartの変更

--- lib/profile.dart
+++ lib/profile.dart
@@ -173,122 +173,120 @@ class _BodyContent extends StatelessWidget {
                           child: Text('+ フレンド追加'),
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.all(8.0),
-                        child: const Text(
-                          'おすすめ',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      StreamBuilder(
-                        stream: users,
-                        builder: (
-                          BuildContext context,
-                          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
-                              snapShot,
-                        ) {
-                          if (snapShot.hasData) {
-                            List<QueryDocumentSnapshot<Map<String, dynamic>>>
-                                data = snapShot.data!.docs;
-                            return SizedBox(
-                              height: 160,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: data.length,
-                                itemBuilder: (context, index) => Card(
-                                  child: Container(
-                                    padding: EdgeInsets.all(10),
-                                    child: SizedBox(
-                                      width: 100,
-                                      child: Column(children: [
-                                        Container(
-                                          height: 15,
-                                          alignment: Alignment.centerRight,
-                                          child: const FittedBox(
-                                            child: Icon(
-                                              Icons.close,
-                                              color: Colors.black26,
-                                            ),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: const EdgeInsets.only(
-                                            top: 0,
-                                            left: 8,
-                                            right: 8,
-                                            bottom: 8,
-                                          ),
-                                          child: CircleAvatar(
-                                            backgroundImage: NetworkImage(
-                                              data[index].data()['imageUrl'],
-                                            ),
-                                          ),
-                                        ),
-                                        Text(
-                                          data[index].data()['firstName'] +
-                                              ' ' +
-                                              data[index].data()['lastName'],
-                                          textAlign: TextAlign.center,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 12,
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          height: 5,
-                                        ),
-                                        SizedBox(
-                                          height: 26,
-                                          child: ElevatedButton(
-                                            style: ElevatedButton.styleFrom(
-                                              primary: Colors.grey[200],
-                                              onPrimary: Colors.black,
-                                              elevation: 0,
-                                              fixedSize: const Size.fromWidth(
-                                                double.maxFinite,
-                                              ),
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius:
-                                                    BorderRadius.circular(13),
-                                              ),
-                                            ),
-                                            onPressed: () {},
-                                            child: const Text(
-                                              '+ 追加',
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ]),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          } else {
-                            return Container();
-                          }
-                        },
-                      ),
-                      TextButton(
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.resolveWith<Color?>(
-                            (Set<MaterialState> states) {
-                              return Colors.black54;
-                            },
-                          ),
-                        ),
-                        onPressed: () => {print('push TextButton')},
-                        child: const Text(
-                          'このアカウントを報告する',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ),
+                      // Container(
+                      //   alignment: Alignment.centerLeft,
+                      //   padding: EdgeInsets.all(8.0),
+                      //   child: const Text(
+                      //     'おすすめ',
+                      //     style: TextStyle(fontWeight: FontWeight.bold),
+                      //   ),
+                      // ),
+                      // StreamBuilder(
+                      //   stream: users,
+                      //   builder: (
+                      //     BuildContext context,
+                      //     AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                      //         snapShot,
+                      //   ) {
+                      //     if (snapShot.hasData) {
+                      //       List<QueryDocumentSnapshot<Map<String, dynamic>>>
+                      //           data = snapShot.data!.docs;
+                      //       return SizedBox(
+                      //         height: 160,
+                      //         child: ListView.builder(
+                      //           scrollDirection: Axis.horizontal,
+                      //           itemCount: data.length,
+                      //           itemBuilder: (context, index) => Card(
+                      //             child: Container(
+                      //               padding: EdgeInsets.all(10),
+                      //               child: SizedBox(
+                      //                 width: 100,
+                      //                 child: Column(children: [
+                      //                   Container(
+                      //                     height: 15,
+                      //                     alignment: Alignment.centerRight,
+                      //                     child: const FittedBox(
+                      //                       child: Icon(
+                      //                         Icons.close,
+                      //                         color: Colors.black26,
+                      //                       ),
+                      //                     ),
+                      //                   ),
+                      //                   Padding(
+                      //                     padding: const EdgeInsets.only(
+                      //                       top: 0,
+                      //                       left: 8,
+                      //                       right: 8,
+                      //                       bottom: 8,
+                      //                     ),
+                      //                     child: CircleAvatar(
+                      //                       backgroundImage: NetworkImage(
+                      //                         data[index].data()['imageUrl'],
+                      //                       ),
+                      //                     ),
+                      //                   ),
+                      //                   Text(
+                      //                     data[index].data()['firstName'],
+                      //                     textAlign: TextAlign.center,
+                      //                     overflow: TextOverflow.ellipsis,
+                      //                     style: const TextStyle(
+                      //                       fontWeight: FontWeight.bold,
+                      //                       fontSize: 12,
+                      //                     ),
+                      //                   ),
+                      //                   const SizedBox(
+                      //                     height: 5,
+                      //                   ),
+                      //                   SizedBox(
+                      //                     height: 26,
+                      //                     child: ElevatedButton(
+                      //                       style: ElevatedButton.styleFrom(
+                      //                         primary: Colors.grey[200],
+                      //                         onPrimary: Colors.black,
+                      //                         elevation: 0,
+                      //                         fixedSize: const Size.fromWidth(
+                      //                           double.maxFinite,
+                      //                         ),
+                      //                         shape: RoundedRectangleBorder(
+                      //                           borderRadius:
+                      //                               BorderRadius.circular(13),
+                      //                         ),
+                      //                       ),
+                      //                       onPressed: () {},
+                      //                       child: const Text(
+                      //                         '+ 追加',
+                      //                         style: TextStyle(
+                      //                           fontWeight: FontWeight.bold,
+                      //                         ),
+                      //                       ),
+                      //                     ),
+                      //                   ),
+                      //                 ]),
+                      //               ),
+                      //             ),
+                      //           ),
+                      //         ),
+                      //       );
+                      //     } else {
+                      //       return Container();
+                      //     }
+                      //   },
+                      // ),
+                      // TextButton(
+                      //   style: ButtonStyle(
+                      //     foregroundColor:
+                      //         MaterialStateProperty.resolveWith<Color?>(
+                      //       (Set<MaterialState> states) {
+                      //         return Colors.black54;
+                      //       },
+                      //     ),
+                      //   ),
+                      //   onPressed: () => {print('push TextButton')},
+                      //   child: const Text(
+                      //     'このアカウントを報告する',
+                      //     style: TextStyle(fontWeight: FontWeight.bold),
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
